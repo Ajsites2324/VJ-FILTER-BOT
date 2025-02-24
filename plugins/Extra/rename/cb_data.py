9312abb879f11268520ae1fe22524f887e1857e7
Ajsites2324 <97234991+Ajsites2324@users.noreply.github.com>
2025-02-24 16:42:15 +0530

    Update cb_data.py

--- plugins/Extra/rename/cb_data.py
+++ plugins/Extra/rename/cb_data.py
@@ -38,7 +38,7 @@ async def doc(bot, update):
             path = await bot.download_media(
                     message=file,
                     progress=progress_for_pyrogram,
-                    progress_args=("**⚠️ Please wait VJ Hack is in processing**", ms, c_time))
+                    progress_args=("**⚠️ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ 𝑨𝒋𝒆𝒆𝒕 𝒃𝒐𝒕𝒔 ɪꜱ ɪɴ ᴘʀᴏᴄᴇꜱꜱɪɴɢ **", ms, c_time))
         except Exception as e:
             await ms.edit(e)
             return 
